b450549f01fb31f9bbb266c89f06822c3ca0e3e2
dkwazhere <dhealth77@gmail.com>
2018-12-06 00:25:21 -0800

    finished pages

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,10 +1,59 @@
 import React, { Component } from "react";
-import { Link } from "react-router-dom";
-import { Jumbotron, Grid, Row, Col, Image, Button } from "react-bootstrap";
+import { Grid, Col, Image } from "react-bootstrap";
 import "./About.css";
 
 export default class About extends Component {
   render() {
-    return <div>ABOUT PAGE</div>;
+    return (
+      <div>
+        <Image src="assets/dog-people.jpg" className="header-image" />
+        <Grid>
+          <Col xs={12} sm={8} smOffset={2}>
+            <Image
+              src="assets/person-1.jpg"
+              className="about-profile-pic"
+              rounded
+            />
+            <h3>Frank the Tank</h3>
+            <p>
+              Let's put some happy little clouds in our world. You are only
+              limited by your imagination. See how easy it is to create a little
+              tree right in your world. How to paint. That's easy. What to
+              paint. That's much harder. But they're very easily killed. Clouds
+              are delicate. If we're gonna walk though the woods, we need a
+              little path. I guess I'm a little weird. I like to talk to trees
+              and animals. That's okay though; I have more fun than most people.
+              This is a fantastic little painting. Zip. That easy. I can't think
+              of anything more rewarding than being able to express yourself to
+              others through painting.
+            </p>
+            <p>
+              Let's put some happy little clouds in our world. You are only
+              limited by your imagination. See how easy it is to create a little
+              tree right in your world. How to paint. That's easy. What to
+              paint. That's much harder. But they're very easily killed. Clouds
+              are delicate. If we're gonna walk though the woods, we need a
+              little path. I guess I'm a little weird. I like to talk to trees
+              and animals. That's okay though; I have more fun than most people.
+              This is a fantastic little painting. Zip. That easy. I can't think
+              of anything more rewarding than being able to express yourself to
+              others through painting.
+            </p>
+            <p>
+              Let's put some happy little clouds in our world. You are only
+              limited by your imagination. See how easy it is to create a little
+              tree right in your world. How to paint. That's easy. What to
+              paint. That's much harder. But they're very easily killed. Clouds
+              are delicate. If we're gonna walk though the woods, we need a
+              little path. I guess I'm a little weird. I like to talk to trees
+              and animals. That's okay though; I have more fun than most people.
+              This is a fantastic little painting. Zip. That easy. I can't think
+              of anything more rewarding than being able to express yourself to
+              others through painting.
+            </p>
+          </Col>
+        </Grid>
+      </div>
+    );
   }
 }
--- src/components/News.jsx
+++ src/components/News.jsx
@@ -1,10 +1,71 @@
 import React, { Component } from "react";
-import { Link } from "react-router-dom";
-import { Jumbotron, Grid, Row, Col, Image, Button } from "react-bootstrap";
+import { Grid, Row, Col, Image } from "react-bootstrap";
 import "./News.css";
 
 export default class News extends Component {
   render() {
-    return <div>NEWS PAGE</div>;
+    return (
+      <div>
+        <Image src="assets/mountain-man.jpg" className="header-image" />
+        <Grid>
+          <h2>News</h2>
+          <Row>
+            <Col xs={12} sm={8} className="main-section">
+              <p>
+                Let's put some happy little clouds in our world. You are only
+                limited by your imagination. See how easy it is to create a
+                little tree right in your world. How to paint. That's easy. What
+                to paint. That's much harder. But they're very easily killed.
+                Clouds are delicate. If we're gonna walk though the woods, we
+                need a little path. I guess I'm a little weird. I like to talk
+                to trees and animals. That's okay though; I have more fun than
+                most people. This is a fantastic little painting. Zip. That
+                easy. I can't think of anything more rewarding than being able
+                to express yourself to others through painting.
+              </p>
+              <p>
+                Let's put some happy little clouds in our world. You are only
+                limited by your imagination. See how easy it is to create a
+                little tree right in your world. How to paint. That's easy. What
+                to paint. That's much harder. But they're very easily killed.
+                Clouds are delicate. If we're gonna walk though the woods, we
+                need a little path. I guess I'm a little weird. I like to talk
+                to trees and animals. That's okay though; I have more fun than
+                most people. This is a fantastic little painting. Zip. That
+                easy. I can't think of anything more rewarding than being able
+                to express yourself to others through painting.
+              </p>
+              <p>
+                Let's put some happy little clouds in our world. You are only
+                limited by your imagination. See how easy it is to create a
+                little tree right in your world. How to paint. That's easy. What
+                to paint. That's much harder. But they're very easily killed.
+                Clouds are delicate. If we're gonna walk though the woods, we
+                need a little path. I guess I'm a little weird. I like to talk
+                to trees and animals. That's okay though; I have more fun than
+                most people. This is a fantastic little painting. Zip. That
+                easy. I can't think of anything more rewarding than being able
+                to express yourself to others through painting.
+              </p>
+            </Col>
+            <Col xs={12} sm={4} className="sidebar-section">
+              <Image src="assets/dog-people.jpg" />
+              <p>
+                Let's put some happy little clouds in our world. You are only
+                limited by your imagination. See how easy it is to create a
+                little tree right in your world. How to paint. That's easy. What
+                to paint. That's much harder. But they're very easily killed.
+                Clouds are delicate. If we're gonna walk though the woods, we
+                need a little path. I guess I'm a little weird. I like to talk
+                to trees and animals. That's okay though; I have more fun than
+                most people. This is a fantastic little painting. Zip. That
+                easy. I can't think of anything more rewarding than being able
+                to express yourself to others through painting.
+              </p>
+            </Col>
+          </Row>
+        </Grid>
+      </div>
+    );
   }
 }
